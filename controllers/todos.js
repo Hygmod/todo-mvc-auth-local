b7799999223153a4a5156635092b2703f0f0ba72
Hygmod <joshsdanton@gmail.com>
2022-09-01 19:12:01 -0700

    update

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,58 +2,54 @@ const Todo = require('../models/Todo')
 const moment = require('moment')
 
 module.exports = {
-    getTodos: async (req,res)=>{
-        try{
-            const todoItems = await Todo.find({userId:req.user.id}).sort('deadline')
-            const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false, })
-<<<<<<< HEAD
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user, moment:moment })
-=======
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user , moment: moment  })
->>>>>>> 1345b986438c2bd4f5611018bb159456b3dbccb1
-        }catch(err){
+    getTodos: async (req, res) => {
+        try {
+            const todoItems = await Todo.find({ userId: req.user.id }).sort('deadline')
+            const itemsLeft = await Todo.countDocuments({ userId: req.user.id, completed: false, })
+            res.render('todos.ejs', { todos: todoItems, left: itemsLeft, user: req.user, moment: moment })
+        } catch (err) {
             console.log(err)
         }
     },
-    createTodo: async (req, res)=>{
+    createTodo: async (req, res) => {
         console.log(req.body)
-        try{
-            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id, deadline: req.body.deadline})
+        try {
+            await Todo.create({ todo: req.body.todoItem, completed: false, userId: req.user.id, deadline: req.body.deadline })
             console.log('Todo has been added!')
             res.redirect('/todos')
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     },
-    markComplete: async (req, res)=>{
-        try{
-            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+    markComplete: async (req, res) => {
+        try {
+            await Todo.findOneAndUpdate({ _id: req.body.todoIdFromJSFile }, {
                 completed: true
             })
             console.log('Marked Complete')
             res.json('Marked Complete')
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     },
-    markIncomplete: async (req, res)=>{
-        try{
-            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+    markIncomplete: async (req, res) => {
+        try {
+            await Todo.findOneAndUpdate({ _id: req.body.todoIdFromJSFile }, {
                 completed: false
             })
             console.log('Marked Incomplete')
             res.json('Marked Incomplete')
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     },
-    deleteTodo: async (req, res)=>{
+    deleteTodo: async (req, res) => {
         console.log(req.body.todoIdFromJSFile)
-        try{
-            await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
+        try {
+            await Todo.findOneAndDelete({ _id: req.body.todoIdFromJSFile })
             console.log('Deleted Todo')
             res.json('Deleted It')
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
